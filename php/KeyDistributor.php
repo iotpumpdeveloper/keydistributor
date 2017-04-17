d42d7001a223b8dab932bb1fd0c8520f1ef0dcc1
jim zike huang <iotpump.developer@gmail.com>
2017-04-17 10:46:13 -0700

    First version of KeyDistributor

---
+++ php/KeyDistributor.php
@@ -0,0 +1,54 @@
+<?php 
+Namespace KeyDistributor;
+
+class KeyDistributor
+{
+
+  private const NUM_OF_SLOTS = 4294967296; //2^32
+
+  private $numOfNodes;
+
+  private function _getNodeIndexForKey($key, $numOfNodes)
+  {
+    $slotsPerNode = (int)(self::NUM_OF_SLOTS / $numOfNodes);
+    $slot = $this->getSlotForKey($key);
+    $index = (int) ($slot / $slotsPerNode);
+    return $index;
+  }
+
+  public function __construct()
+  {
+    $this->numOfNodes = 0;
+  }
+
+  public function setNumOfNodes($numOfNodes)
+  {
+    $this->numOfNodes = $numOfNodes;
+  }
+
+  public function getSlotForKey($key)
+  {
+    $slot = crc32($key) % self::NUM_OF_SLOTS;
+    return $slot; 
+  }
+
+  public function getNodeIndexForKey($key)
+  {
+    return $this->_getNodeIndexForKey($key, $this->numOfNodes);
+  }
+
+  public function getNodeForKey($key)
+  {
+    $index = $this->_getNodeIndexForKey($key, $this->numOfNodes);
+    return $this->nodes[$index];
+  }
+
+  public function getMigrationPathForKey($key)
+  {
+    $nodes = array();
+    for ($i = 1; $i <= $this->numOfNodes; $i++) {
+      $nodes[] = $this->_getNodeIndexForKey($key, $i); 
+    }
+    return implode(".", $nodes);
+  }
+}
