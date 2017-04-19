778cbff6b954cc6ecc5e99b5bf43da6f54b78f6d
jim zike huang <iotpump.developer@gmail.com>
2017-04-18 22:54:40 -0700

    add slot migration examples

---
+++ php/examples/slot_migration_preparation.php
@@ -0,0 +1,26 @@
+<?php
+spl_autoload_register(function($class) {
+  require (__DIR__.'/../'.str_replace('\\','/',$class).'.php');
+});
+
+//first node map
+$nodeMap_1 = array(
+  's1' => array('weight' => 4),
+  's2' => array('weight' => 16),
+  's3' => array('weight' => 32)
+);
+
+//second node map, we remove s1, and add a new server s4
+$nodeMap_2 = array(
+  's2' => array('weight' => 16),
+  's3' => array('weight' => 32),
+  's4' => array('weight' => 64)
+);
+
+$slot = rand(0,65535);
+
+$map = new KeyDistributor\WeightedNodeMap($nodeMap_1);
+echo "before migration, slot $slot is at server: ".$map->getNodeForSlot($slot)."\n";
+
+$map = new KeyDistributor\WeightedNodeMap($nodeMap_2);
+echo "after migration, slot $slot will be at server: ".$map->getNodeForSlot($slot)."\n";
